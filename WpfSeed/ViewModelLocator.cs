e01fa54133aa6db4b8a2de47b0f78a032e138df8
deaddog <msl@neasenergy.com>
2018-01-11 15:16:46 +0100

    Introduced the ViewModelLocator.

---
+++ WpfSeed/ViewModelLocator.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using WpfSeed.ViewModels;
+
+namespace WpfSeed
+{
+    public class ViewModelLocator
+    {
+        private static IContainer CreateDesignContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new ViewModelsModule());
+
+            return builder.Build();
+        }
+
+        private readonly IContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = CreateDesignContainer();
+        }
+
+        public MainViewModel MainViewModel => _container.Resolve<MainViewModel>();
+    }
+}
