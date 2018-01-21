1b65b511efc4b34d4fa00e7aebd10840242992d3
deaddog <msl@neasenergy.com>
2018-01-21 21:25:32 +0100

    Extended ValueConverter with a parameter version.

--- WpfSeed/WpfSeed/Converters/ValueConverter.cs
+++ WpfSeed/WpfSeed/Converters/ValueConverter.cs
@@ -39,4 +39,50 @@ namespace WpfSeed.Converters
                 throw new ArgumentException($"{GetType()?.Name}.ConvertBack expects type {typeof(TOut)?.Name} as input, {value.GetType()?.Name} is not supported.", nameof(value));
         }
     }
+
+    public abstract class ValueConverter<TIn, TOut, TParam> : IValueConverter
+    {
+        public abstract TOut Convert(TIn value, TParam parameter, CultureInfo culture);
+        public abstract TIn ConvertBack(TOut value, TParam parameter, CultureInfo culture);
+
+        private readonly bool _testTargetType;
+
+        public ValueConverter(bool testTargetType = true)
+        {
+            _testTargetType = testTargetType;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (_testTargetType && targetType != typeof(TOut) && !targetType.IsSubclassOf(typeof(TOut)))
+                throw new InvalidCastException($"{GetType()?.Name}.Convert can only convert to type {typeof(TOut)?.Name}, {targetType?.Name} is expected.");
+            else if (value is null)
+                return Convert((TIn)value, GetParameter(parameter), culture);
+            else if (value is TIn inValue)
+                return Convert(inValue, GetParameter(parameter), culture);
+            else
+                throw new ArgumentException($"{GetType()?.Name}.Convert expects type {typeof(TIn)?.Name} as input, {value.GetType()?.Name} is not supported.", nameof(value));
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (_testTargetType && targetType != typeof(TIn) && !targetType.IsSubclassOf(typeof(TIn)))
+                throw new InvalidCastException($"{GetType()?.Name}.ConvertBack can only convert to type {typeof(TIn)?.Name}, {targetType?.Name} is expected.");
+            else if (value is null)
+                return ConvertBack((TOut)value, GetParameter(parameter), culture);
+            else if (value is TOut outValue)
+                return ConvertBack(outValue, GetParameter(parameter), culture);
+            else
+                throw new ArgumentException($"{GetType()?.Name}.ConvertBack expects type {typeof(TOut)?.Name} as input, {value.GetType()?.Name} is not supported.", nameof(value));
+        }
+
+        private TParam GetParameter(object parameter)
+        {
+            if (parameter is null)
+                return (TParam)parameter;
+            else if (parameter is TParam param)
+                return param;
+            else
+                throw new ArgumentException("Unable to convert parameter to command type.", nameof(parameter));
+        }
+    }
 }
